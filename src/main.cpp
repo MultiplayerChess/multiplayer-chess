476055784a0c361b5b61dbaeca760dcb6fa197c3
Allen Zhao <allenz0509@gmail.com>
2023-11-23 02:04:11 -0500

    Added prereqs

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio>
+#include<stdlib>
+#include<regex>
+#include<iostream>
+#include<string>
+
+// using namespace std;
+// using std namespace is bad practice but I'll keep this here
+
+
+int main(int argc, char **argv) {
+	std::cout << "Test" << std::endl;
+	return 0;
+}
