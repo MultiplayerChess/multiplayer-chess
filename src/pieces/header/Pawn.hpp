39c4039650acaab52acd849f7f66ae4472f6b3ab
Allen Zhao <allenz0509@gmail.com>
2023-11-23 03:37:57 -0500

    writes header for Board.hpp, Pieces.hpp, Knight.hpp, Pawn.hpp

---
+++ src/pieces/header/Pawn.hpp
@@ -0,0 +1,39 @@
+#include<stdio>
+#include<stdlib>
+#include<string>
+#include<regex>
+
+#ifndef PAWN_HPP
+#define PAWN_HPP
+
+
+class Pawn: public Pieces{
+
+
+	/* 
+	 * param: position and color
+	 * output: creates constructor for pawn
+	 */
+	Pawn(int[2] position, std::string color);
+
+
+	/*
+	 * output: all the possible moves the piece can do while still being a legal move
+	 * return: 2D array of all the possible moves.
+	 * note: the pawn can move diagnonally when capturing, and can en pessant. Logic for checking if the move is legal should be defined in the board class.
+	 * note: pawn cannot move if there is a piece right ahead of it.
+	 */
+	int[][] getPossibleMoves();
+	
+	/*
+	 * param: the piece the player wishes to promote to.
+	 * param: can be Knight, Rook, Bishop, or Queen.
+	 * output: replaces the pawn with the desired promoted piece.
+	 * note: you have to force the user to choose, or else the game cannot continue
+	 */
+	void promotion(std::string piece);
+
+}
+
+
+#endif
