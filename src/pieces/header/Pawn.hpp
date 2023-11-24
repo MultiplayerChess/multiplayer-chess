0561777d21ac885abc7eff08ec55a79a4a5cea7b
Allen Zhao <allenz0509@gmail.com>
2023-11-23 22:48:25 -0500

    fixes mistake in the Pawn header

--- src/pieces/header/Pawn.hpp
+++ src/pieces/header/Pawn.hpp
@@ -1,27 +1,27 @@
 #include "Pieces.hpp"
-
 #ifndef PAWN_HPP
 #define PAWN_HPP
 class Pawn: public Pieces{
-	 
+	
+	public:
 	/* 
 	 * param: position and color
 	 * output: creates constructor for pawn
 	 */
-	Pawn(int[2] position, std::string color);
-
+	Pawn(int* position, std::string color);
+	
 	/*
 	 * output: destructor
 	 */
 	~Pawn();
-
+	
 	/*
 	 * output: all the possible moves the piece can do while still being a legal move
 	 * return: 2D array of all the possible moves.
 	 * note: the pawn can move diagnonally when capturing, and can en pessant. Logic for checking if the move is legal should be defined in the board class.
 	 * note: pawn cannot move if there is a piece right ahead of it.
 	 */
-	int[][] getPossibleMoves() const;
+	int** getPossibleMoves() const;
 
 	/*
 	 * param: the piece the player wishes to promote to.
