113f7c2db7213a83d326cba5db4ab6c24a5ecee9
Allen Zhao <allenz0509@gmail.com>
2023-12-04 00:34:55 -0500

    mmaybe I forgot the queen header

--- src/pieces/header/Queen.hpp
+++ src/pieces/header/Queen.hpp
@@ -18,7 +18,7 @@ class Queen: public Pieces {
 		 * note: queen can move horizontal, vertical, and diagonal.
 		 */
 
-    std::vector<std::vector<int>> getPossibleMoves() const;
+    std::vector<std::vector<int>> getPossibleMoves(std::vector<Pieces> ownPieces, std::vector<Pieces> enemyPieces) const;
 }
 
 #endif
